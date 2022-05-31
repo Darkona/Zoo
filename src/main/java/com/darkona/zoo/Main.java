5031c4b9dd1b79d1f5d4867dbac5d40672147d77
Darkona <Javier.Darkona@gmail.com>
2022-05-30 20:18:06 -0400

    Log level as first argument

--- src/main/java/com/darkona/zoo/Main.java
+++ src/main/java/com/darkona/zoo/Main.java
@@ -12,14 +12,14 @@ public class Main {
 
     public static void main(String[] args) {
         Logger.debug("Initializing...");
-        configureTinyLog();
+        configureTinyLog(args != null && args.length> 0 &&args[0] != null ? args[0] : "warning" );
         new Thread(new Loop(new Simulation(new Size( Configuration.getInstance().getWidth(),  Configuration.getInstance().getHeight())))).start();
     }
 
-    private static void configureTinyLog() {
+    private static void configureTinyLog(String level) {
         Logger.debug("Configuring logger...");
         Configurator.currentConfig().removeAllWriters().addWriter(new ColoredConsoleWriter()).activate();
-        Configurator.currentConfig().level(Level.INFO).activate();
+        Configurator.currentConfig().level(Level.valueOf(level.toUpperCase())).activate();
         Configurator.currentConfig()
                 .formatPattern("{{level}:|min-size=8} {date} - {class_name}.class>{method}>{line}|  |{message}")
                 .activate();
